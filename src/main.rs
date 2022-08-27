19d94c068e2efd0527b5d02b82eb1252f86e20d5
Ziyuan Han <hanziyuan08@outlook.com>
2022-08-27 17:58:46 +0800

    First commit

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
-fn main() {
-    println!("Hello, world!");
+use std::collections::HashMap;
+use regex::Regex;
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let resp = reqwest::blocking::get("https://www.baidu.com")?
+        .json::<HashMap<String, String>>()?;
+    println!("{:#?}", resp);
+
+    Ok(())
 }
