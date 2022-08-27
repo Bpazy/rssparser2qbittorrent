b3df9a053ecb629dcfa97505c5fd64de7e0cdcbd
Ziyuan Han <hanziyuan08@outlook.com>
2022-08-28 02:13:17 +0800

    feat: support command line arguments

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # rssparser2qbittorrent
 
 Status: developing
+
+## Examples
+
+### Get magnet uri from rss desription
+
+```
+rssparser2qbittorrent.exe --url https://rsshub.app/example --best-regex 寻梦环游记
+```
--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,74 @@
+use std::error::Error;
+
 use clap::Parser;
+use regex::Regex;
 use rss::Channel;
+use scraper::{Html, Selector};
 
 #[derive(Parser, Debug)]
 #[clap(author, version, about, long_about = None)]
 struct Args {
     #[clap(short, long, value_parser)]
     url: String,
+    #[clap(short, long, value_parser)]
+    best_regex: String,
 }
 
-fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let args = Args::parse();
-    println!("args.name: {}", args.url);
+fn main() -> Result<(), Box<dyn Error>> {
+    run(&Args::parse());
+    Ok(())
+}
 
-    let resp = reqwest::blocking::get(args.url)?.bytes()?;
-    let channel = Channel::read_from(&resp[..])?;
+fn run(args: &Args) {
+    let channel = get_rss(&args).expect("Failed to get rss content");
     for item in channel.items {
-        println!("{}", item.title.unwrap());
+        println!("Title: {}\n\tBest Url: {}", item.title.unwrap(), get_best_url(item.description.unwrap(), args).unwrap_or(String::from("None best found")));
+    }
+}
+
+#[derive(Debug)]
+struct Magnet {
+    uri: String,
+    title: String,
+}
+
+impl Magnet {
+    fn new(title: String, uri: String) -> Self {
+        Magnet { title, uri }
+    }
+}
+
+
+fn get_best_url(description: String, args: &Args) -> Option<String> {
+    let fragment = Html::parse_fragment(description.as_str());
+    let selector = Selector::parse(r#"a[href]"#).unwrap();
+    let mut v = Vec::new();
+    for a in fragment.select(&selector) {
+        match a.value().attr("href") {
+            Some(href) => {
+                if href.starts_with("magnet") {
+                    v.push(Magnet::new(a.text().collect::<String>(), href.to_string()));
+                }
+            }
+            _ => continue
+        }
     }
 
-    Ok(())
+    let re = Regex::new(args.best_regex.as_str()).unwrap();
+    let mut best_magnet = None;
+    for magnet in v {
+        if re.find(magnet.title.as_str()).is_some() {
+            best_magnet = Some(magnet)
+        }
+    }
+    return match best_magnet {
+        Some(magnet) => Some(magnet.uri),
+        _ => None
+    };
+}
+
+fn get_rss(args: &Args) -> Result<Channel, Box<dyn Error>> {
+    let resp = reqwest::blocking::get(&args.url)?.bytes()?;
+    let channel = Channel::read_from(&(resp[..]))?;
+    Ok(channel)
 }
