919ae760c58c987048352217d2939a9b4a055812
Ziyuan Han <hanziyuan08@outlook.com>
2022-08-27 23:31:17 +0800

    feat: support command line arguments

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,22 @@
-use std::collections::HashMap;
-use regex::Regex;
+use clap::Parser;
+use rss::Channel;
+
+#[derive(Parser, Debug)]
+#[clap(author, version, about, long_about = None)]
+struct Args {
+    #[clap(short, long, value_parser)]
+    url: String,
+}
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let resp = reqwest::blocking::get("https://www.baidu.com")?
-        .json::<HashMap<String, String>>()?;
-    println!("{:#?}", resp);
+    let args = Args::parse();
+    println!("args.name: {}", args.url);
+
+    let resp = reqwest::blocking::get(args.url)?.bytes()?;
+    let channel = Channel::read_from(&resp[..])?;
+    for item in channel.items {
+        println!("{}", item.title.unwrap());
+    }
 
     Ok(())
 }
